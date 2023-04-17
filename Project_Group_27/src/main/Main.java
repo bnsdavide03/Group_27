f355ac52e44f44bb668784a531240debaa52cac0
gmazzolen1 <gabriel.mazzoleni24@gmail.com>
2023-04-17 14:25:48 +0200

    create class Map

--- Project_Group_27/src/main/Main.java
+++ Project_Group_27/src/main/Main.java
@@ -3,10 +3,6 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		//prova commit
-		//prova token
-		//ciao prova 
-		//prova 
 
 	}
 
---
+++ Project_Group_27/src/main/Map.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Map {
+	//Tile[][] map= new Tile[9][9]; //verificare correttezza sintassi dopo aver creato classe Tile
+	void Map()
+	{
+		//random generator per settare tiles sulla mappa, serve la classe Tiles
+	}
+	
+	void fillMap()
+	{
+		
+	}
+	
+	void verifyMap()
+	{
+		
+	}
+	/*Tile[][] getMap()
+	{
+		return this.map;
+	}*/
+}
