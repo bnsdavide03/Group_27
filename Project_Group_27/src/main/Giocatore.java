770fb5eaf467f254acefd139da692b6f6d6a2839
mtironi <m.tironi17@studenti.unibg.it>
2023-04-17 18:51:06 +0200

    Attributi e metodi(solo definiti) classe Giocatore

--- Project_Group_27/src/main/Giocatore.java
+++ Project_Group_27/src/main/Giocatore.java
@@ -1,5 +1,43 @@
 package main;
 
 public class Giocatore {
-	//modifica
+	private int number;
+	private String name;
+	private int points;
+	private boolean chair;
+	//private PersonalCard personalCard quando sarà creata la classe
+	private Library library;
+	
+	void Giocatore(String name) {
+	
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+	
+	public int getPoints() {
+		return this.points;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int calculateTotalPoints()
+	{
+		return this.points;
+	}
+	
+	void setChairTrue() {
+		this.chair=true;
+	}
+	
+	int incrementPoints(int points) {
+		return this.points;
+	}
+	/*int play(){
+	 
+	}*/
+	
 }
