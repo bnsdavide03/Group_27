d7a7b81dd80751caffeeda2de754b920f06fae1b
Fustioni <l.fustioni4@studenti.unibg.it>
2023-04-27 12:25:49 +0200

    interfaces

---
+++ Project_Group_27/src/interfaces/start.java
@@ -0,0 +1,47 @@
+package interfaces;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class start extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					start frame = new start();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public start() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 689, 447);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel benvenuto = new JLabel("benvenuto su myshelfie");
+		benvenuto.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		contentPane.add(benvenuto);
+	}
+
+}
