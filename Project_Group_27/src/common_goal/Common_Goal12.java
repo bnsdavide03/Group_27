13eeb4798685b8a5662d327f12124aacbbd15e58
gmazzolen1 <84225864+gmazzolen1@users.noreply.github.com>
2023-05-04 12:56:02 +0200

    Update Common_Goal12.java
    update Common_Goal12

--- Project_Group_27/src/common_goal/Common_Goal12.java
+++ Project_Group_27/src/common_goal/Common_Goal12.java
@@ -1,6 +1,9 @@
 package common_goal;
 
+import java.util.ArrayList;
+
 import main.Library;
+import main.Position;
 
 public class Common_Goal12 extends Common_Goal{
 	
@@ -12,7 +15,18 @@ public class Common_Goal12 extends Common_Goal{
 
 	@Override
 	public boolean verify_goal(Library libreria) {
-		// TODO Auto-generated method stub
+		int[] nTilesOnColumns=new int[5];
+		nTilesOnColumns[0]=calculateNumberTilesColumn(libreria,0);
+		nTilesOnColumns[1]=calculateNumberTilesColumn(libreria,1);
+		nTilesOnColumns[2]=calculateNumberTilesColumn(libreria,2);
+		nTilesOnColumns[3]=calculateNumberTilesColumn(libreria,3);
+		nTilesOnColumns[4]=calculateNumberTilesColumn(libreria,4);
+		if(nTilesOnColumns[0]<nTilesOnColumns[1]&&nTilesOnColumns[1]<nTilesOnColumns[2]&&nTilesOnColumns[2]<nTilesOnColumns[3]&&nTilesOnColumns[3]<nTilesOnColumns[4]) {
+			return true;
+		}
+		else if(nTilesOnColumns[4]<nTilesOnColumns[3]&&nTilesOnColumns[3]<nTilesOnColumns[2]&&nTilesOnColumns[2]<nTilesOnColumns[1]&&nTilesOnColumns[1]<nTilesOnColumns[0]) {
+			return true;
+		}
 		return false;
 	}
 	@Override
@@ -25,6 +39,19 @@ public class Common_Goal12 extends Common_Goal{
 		for (int i=0; i<4; i++) {
 			System.out.println(this.remaningCards[i]);
 		}
+	}
+	
+	private int calculateNumberTilesColumn(Library libreria, int column){
+		int nTiles=0;
+		for(int i=5;i>=0;i--){
+			if(libreria.getTile(new Position(i,column))!=null) {
+				nTiles++;
+			}
+			else {
+				return nTiles;
+			}
+		}
+		return nTiles;
 	}
 
 }
