216dc6abbc6e3c6bc46f7a6ce25a8989a66e774e
gmazzolen1 <gabriel.mazzoleni24@gmail.com>
2023-05-08 15:55:42 +0200

    update CommonGoal_10 and test it

--- Project_Group_27/src/common_goal/Common_Goal10.java
+++ Project_Group_27/src/common_goal/Common_Goal10.java
@@ -1,5 +1,6 @@
 package common_goal;
 
+import main.Color;
 import main.Library;
 import main.Position;
 
@@ -13,36 +14,45 @@ public class Common_Goal10 extends Common_Goal{
 
 	@Override
 	public boolean verify_goal(Library library) {
-		int count=0;
-		int controllo=0;
-		for (int i=0;i<7;i++)
-		{
-			for (int k=0;k<6&&controllo==0;k++)
-			{
-				Position p= new Position(i,k);
-				for(int j=k;j<6;j++)
-				{
-					Position p1= new Position(k,j);
-					if(library.getTile(p).getColor()==library.getTile(p1).getColor())
-					{
-						controllo=-1;
-						break;
+		int count = 0;
+
+		for (int i = 0; i < 6; i++) {
+			int array[] = new int[6];
+			for (int k = 0; k < 5; k++) {
+				Position p = new Position(i, k);
+				if (library.getTile(p) != null) {
+					if (library.getTile(p).getColor() == Color.PINK) {
+						array[0]++;
+					} else if (library.getTile(p).getColor() == Color.BLUE) {
+						array[1]++;
+					} else if (library.getTile(p).getColor() == Color.LIGHT_BLUE) {
+						array[2]++;
+					} else if (library.getTile(p).getColor() == Color.GREEN) {
+						array[3]++;
+					} else if (library.getTile(p).getColor() == Color.YELLOW) {
+						array[4]++;
+					} else if (library.getTile(p).getColor() == Color.WHITE) {
+						array[5]++;
 					}
+				} else {
+					break;
 				}
 			}
-			if (controllo==0)
-			{
+			//se uno è 0, lo mettiamo a uno per far funzionare l'if sottostante, se invece due sono a 0 ritornerà false
+			for(int n=0;n<6;n++) {
+				if(array[n]==0)
+				{
+					array[n]++;
+					break;
+				}
+			}
+			if (array[0] == 1 && array[1] == 1 && array[2] == 1 && array[3] == 1 && array[4] == 1 && array[5] == 1) {
 				count++;
 			}
-			else
-			{
-				controllo=0;
-			}
-			if (count==2)
-			{
+			if (count >= 2) {
 				return true;
 			}
-			
+
 		}
 		return false;
 	}
--- Project_Group_27/src/main/Main.java
+++ Project_Group_27/src/main/Main.java
@@ -3,23 +3,27 @@ import common_goal.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Library lib=new Library();
+		/*Library lib=new Library();
 		
-		/*lib.library[0][0]=new Tile(new Position(0,0),Color.BLUE);
-		lib.library[1][0]=new Tile(new Position(1,0),Color.PINK);
-		lib.library[2][0]=new Tile(new Position(2,0),Color.WHITE);
-		lib.library[3][0]=new Tile(new Position(3,0),Color.YELLOW);
-		lib.library[4][0]=new Tile(new Position(4,0),Color.GREEN);
-		lib.library[5][0]=new Tile(new Position(5,0),Color.LIGHT_BLUE);
+		lib.library[0][0]=new Tile(new Position(0,0),Color.BLUE);
+		lib.library[0][1]=new Tile(new Position(0,1),Color.WHITE);
+		lib.library[0][2]=new Tile(new Position(0,2),Color.WHITE);
+		lib.library[0][3]=new Tile(new Position(0,3),Color.YELLOW);
+		lib.library[0][4]=new Tile(new Position(0,4),Color.GREEN);
 
 		
 		
-		lib.library[0][1]=new Tile(new Position(0,1),Color.BLUE);
-		lib.library[1][1]=new Tile(new Position(1,1),Color.WHITE);
-		lib.library[2][1]=new Tile(new Position(2,1),Color.WHITE);
-		lib.library[3][1]=new Tile(new Position(3,1),Color.YELLOW);
-		lib.library[4][1]=new Tile(new Position(4,1),Color.GREEN);
-		lib.library[5][1]=new Tile(new Position(5,1),Color.LIGHT_BLUE);
+		lib.library[1][0]=new Tile(new Position(1,0),Color.BLUE);
+		lib.library[1][1]=new Tile(new Position(1,1),Color.PINK);
+		lib.library[1][2]=new Tile(new Position(1,2),Color.WHITE);
+		lib.library[1][3]=new Tile(new Position(1,3),Color.YELLOW);
+		lib.library[1][4]=new Tile(new Position(1,4),Color.GREEN);
+		
+		lib.library[2][0]=new Tile(new Position(2,0),Color.BLUE);
+		lib.library[2][1]=new Tile(new Position(2,1),Color.PINK);
+		lib.library[2][2]=new Tile(new Position(2,2),Color.WHITE);
+		lib.library[2][3]=new Tile(new Position(2,3),Color.YELLOW);
+		lib.library[2][4]=new Tile(new Position(2,4),Color.GREEN);*/
 
 		
 		/*lib.library[2][1]=new Tile(new Position(2,1),Color.BLUE);
@@ -33,8 +37,8 @@ public class Main {
 		lib.library[3][3]=new Tile(new Position(3,3),Color.BLUE);
 		lib.library[3][4]=new Tile(new Position(3,4),Color.BLUE);
 		lib.library[3][0]=new Tile(new Position(3,0),Color.BLUE);*/
-		Common_Goal09 goal = new Common_Goal09(4);
-		System.out.println(goal.verify_goal(lib));
+		/*Common_Goal10 goal = new Common_Goal10(4);
+		System.out.println(goal.verify_goal(lib));*/
 		/*lib.library[5][1]=new Tile(new Position(5,1),Color.BLUE);
 		
 		lib.library[0][2]=new Tile(new Position(0,2),Color.BLUE);
